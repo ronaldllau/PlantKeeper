8f407a1fe47b096f3ff0694b64478d84d52f93a5
ronaldllau <ronaldllau@gmail.com>
2025-09-03 17:08:13 +0800

    Initial Commit

---
+++ PlantKeeper/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PlantKeeper
+//
+//  Created by Ronald on 3/9/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PlantKeeper/PlantKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlantKeeperApp.swift
+//  PlantKeeper
+//
+//  Created by Ronald on 3/9/2025.
+//
+
+import SwiftUI
+
+@main
+struct PlantKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
